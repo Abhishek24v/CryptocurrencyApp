3d72b1c91ee7d51c95e99cefa284a9fab1827dd9
Abhishek Vishwakarma <avish024@gmail.com>
2022-09-22 10:28:14 +0530

    Commit Cryptocurrency module

---
+++ app/src/main/java/com/example/cryptocurrencyapp/presentation/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.cryptocurrencyapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.cryptocurrencyapp.presentation.coin_detail.CoinDetailScreen
+import com.example.cryptocurrencyapp.presentation.coin_list.components.CoinListScreen
+import com.example.cryptocurrencyapp.presentation.ui.theme.CryptocurrencyAppYTTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CryptocurrencyAppYTTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    val navController = rememberNavController()
+                    NavHost(navController = navController,
+                        startDestination = Screen.CoinListScreen.route) {
+                        composable(
+                            route = Screen.CoinListScreen.route
+                        ) {
+                            CoinListScreen(navController)
+                        }
+                        composable(
+                            route = Screen.CoinDetailScreen.route + "/{coinId}"
+                        ) {
+                            CoinDetailScreen()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
